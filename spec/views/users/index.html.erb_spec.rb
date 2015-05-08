06b235baffe80231d1beb048452b4bc6ebd23254
Yason Khaburzaniya <yason.khabur@yahoo.com>
2015-05-07 23:54:35 -0700

    fixing the user index view spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe "users/index", type: :view do
   before(:each) do
     assign(:users, [
       User.create!(
-        :name => ""
+        :name => "User1"
       ),
       User.create!(
-        :name => ""
+        :name => "User2"
       )
     ])
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "".to_s, :count => 4
+    assert_select "tr>td", :text => /User./, :count => 2
   end
 end
